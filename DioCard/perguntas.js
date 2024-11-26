33aaff30a7a2cc2061dd59d585292081e2f647ba
Diogo Santana <00001135647847sp@al.educacao.sp.gov.br>
2024-11-25 21:29:35 -0300

    Add files via upload

---
+++ DioCard/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Java script?',
+    'O Java Script é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Argentina?',
+    'A capital da Argentina é Buenos Ares'
+)
+
+criaCartao(
+    'Conhecimento geral',
+    'qual o valor de Pi?',
+    'aproximadamente 3,14'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
